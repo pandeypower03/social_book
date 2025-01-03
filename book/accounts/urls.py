f781d39d2805e3400eb78fa5d3bb383b34f7ff65
pandeypower03 <priyanka.pandey772003@gmail.com>
2025-01-03 12:05:07 +0530

    login page done

--- book/accounts/urls.py
+++ book/accounts/urls.py
@@ -5,5 +5,5 @@ from django.contrib import admin
 urlpatterns = [
     
     path("signup/",views.signup_view,name="signup"),
-    
+    path('login/', views.login_view, name='login'),
 ]
--- book/accounts/views.py
+++ book/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 
 # Create your views here.
@@ -10,3 +11,17 @@ def signup_view(request):
         User.objects.create_user(username=username, password=password)
         return HttpResponse("Signup successful!")
     return render(request,'home.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return HttpResponse("Login successful!")
+        else:
+            return HttpResponse("Invalid credentials! <br> <a href='/signup/'>Signup here</a>")
+
+    return render(request, 'login.html')
