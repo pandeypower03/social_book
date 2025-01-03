ef2cf56cea5570d81aa4362aee66c9bc49658196
pandeypower03 <priyanka.pandey772003@gmail.com>
2025-01-03 11:55:35 +0530

    signup done

--- book/accounts/views.py
+++ book/accounts/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 
 # Create your views here.
-def home(request):
+def signup_view(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        password = request.POST['password']
+        User.objects.create_user(username=username, password=password)
+        return HttpResponse("Signup successful!")
     return render(request,'home.html')
